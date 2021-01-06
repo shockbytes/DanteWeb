cb9704e2476cd5982cb5d735d1c5b13b08d8d5ec
shockbytes <mescht93@gmail.com>
2021-01-06 14:06:57 +0100

    Trigger another build

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -3,7 +3,7 @@ import './MainPage.css';
 function MainPage() {
     return (
         <div className="App">
-            <h1>Dante Web</h1>
+            <h1>Dante Web App</h1>
             <p>Coming soon...</p>
         </div>
     );
