94baf47778c3bc18428d02f941903e3dca18a84d
shockbytes <mescht93@gmail.com>
2021-01-06 21:47:51 +0100

    Change theme color and fix terms link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={MainPage}/>
-                    <Route path="/terms" component={TermsPage}/>
+                    <Route exact path="/terms" component={TermsPage}/>
                 </Switch>
             </BrowserRouter>
         )
